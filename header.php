a86fb90ebb76b1370057b776c605c70902d613d9
Patrick Roche <patrick.l.roche@gmail.com>
2015-11-12 08:47:20 -0800

    Change index to PHP.  Add PHP header file

---
+++ header.php
@@ -0,0 +1,16 @@
+<?php
+echo "
+        <div class=\"row text-center pagination-centered adjust-vert\">
+            <div class=\"col-md-4\">
+                <div class=\"thumbnail\">
+                    <img class=\"header-logo\" src=\"images/logo.png\" alt=\"Masterworks Concert Chorale\">
+                </div>
+            </div>
+            <div class=\"col-md-8\">
+                <div class=\"thumbnail tagline\">
+                <h3 class=\"\">Uplifting Voices.  Uplifting Spirits.  Uplifting Pittsburgh.</h3>
+                </div>
+            </div>
+        </div>
+"
+?>
--- index.php
+++ index.php
@@ -49,20 +49,7 @@
     <!-- Start Bootstrap container -->
     <div class="container">
         <!--Start Header-->
-        <div class="row text-center pagination-centered adjust-vert">
-
-            <div class="col-md-4">
-                <div class="thumbnail">
-                    <img class="header-logo" src="images/logo.png" alt="Masterworks Concert Chorale">
-                </div>
-            </div>
-            <div class="col-md-8">
-                <div class="thumbnail tagline">
-                <h3 class="">Uplifting Voices.  Uplifting Spirits.  Uplifting Pittsburgh.</h3>
-                </div>
-            </div>
-
-        </div>
+			<?php include 'header.php';?>
         <!--End Header-->
         <!--Start Navbar-->
         <nav class="navbar navbar-inverse">
